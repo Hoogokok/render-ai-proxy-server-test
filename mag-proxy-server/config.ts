8faaa3ee2662708a7531194920c06040047fc14a
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-25 22:19:11 +0900

    feat: 캐시를 1시간을 수정

--- mag-proxy-server/config.ts
+++ mag-proxy-server/config.ts
@@ -4,5 +4,5 @@ export const API_KEY = Deno.env.get("JINA_API_KEY")
 export const API_URL = Deno.env.get("JINA_API_URL")
 export const CACHE_KEY = new URL(API_URL);
 export const CACHE_NAME = "fangoria-articles-cache";
-export const CACHE_TTL = 600000; // 10분 (밀리초 단위)
+export const CACHE_TTL = 3600000; // 1시간
 export const PORT = Deno.env.get("PORT") || "3031";
