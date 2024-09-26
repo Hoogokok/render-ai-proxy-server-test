f74e9e82cc6733f38c66d2507bacca30a1c8262b
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-26 16:51:19 +0900

    feat: API_URL에 사용할 환경 변수 이름 수정

--- mag-proxy-server/config.ts
+++ mag-proxy-server/config.ts
@@ -1,7 +1,6 @@
 import "jsr:@std/dotenv/load";
 
-export const API_KEY = Deno.env.get("JINA_API_KEY") 
-export const API_URL = Deno.env.get("JINA_API_URL")
+export const API_URL = Deno.env.get("SCRAPING_URL")
 export const CACHE_KEY = new URL(API_URL);
 export const CACHE_NAME = "fangoria-articles-cache";
 export const CACHE_TTL = 3600000; // 1시간
