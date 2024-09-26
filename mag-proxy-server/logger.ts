97c4a8973925679435db7181992eabf6a3a8a9ad
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-26 17:09:00 +0900

    feat: 구조화된 로깅 시스템 구현 (logger.ts)
    - 커스텀 로거 함수 생성
    - 로그 레벨 (DEBUG, INFO, WARNING, ERROR) 지원
    - 콘솔 출력을 통한 간단한 로깅 구현
    - 전역에서 사용 가능한 로거 인스턴스 생성

---
+++ mag-proxy-server/logger.ts
@@ -0,0 +1,15 @@
+import * as log from "https://deno.land/std@0.177.0/log/mod.ts";
+
+await log.setup({
+  handlers: {
+    console: new log.handlers.ConsoleHandler("DEBUG"),
+  },
+  loggers: {
+    default: {
+      level: "DEBUG",
+      handlers: ["console"],
+    },
+  },
+});
+
+export default log;
