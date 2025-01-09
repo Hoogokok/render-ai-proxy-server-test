454d61ec3f5b3bc5703968fb9ba793b0996b30ca
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2025-01-10 00:01:02 +0900

    feat: add SCRAPED_URL to configuration for enhanced article fetching

--- mag-proxy-server/config.ts
+++ mag-proxy-server/config.ts
@@ -2,6 +2,7 @@ import "jsr:@std/dotenv/load";
 
 export const API_KEY = Deno.env.get("JINA_API_KEY") 
 export const API_URL = Deno.env.get("JINA_API_URL")
+export const SCRAPED_URL = Deno.env.get("SCRAPED_URL")
 export const CACHE_KEY = new URL(API_URL);
 export const CACHE_NAME = "fangoria-articles-cache";
 export const CACHE_TTL = 3600000; // 1시간
